4d684ee0f2b48f6cde7c434b0071b80006b5e929
Luís Ferreira <contact@lsferreira.net>
2019-06-06 15:24:32 +0100

    initial code
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ source/app.d
@@ -0,0 +1,17 @@
+module app;
+
+int main() {
+	import std.stdio : writeln;
+
+	foreach(i; 1 .. 100)
+	{
+		if(i % 3 == 0)
+			writeln("Foo");
+		else if(i % 5 == 0)
+			writeln("Bar");
+		else
+			writeln(i);
+	}
+
+	return 0;
+}
